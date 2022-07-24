919bbaa0e2c8e2e6049e3f58d418729df0940bba
L2M <loren.milliman@gmail.com>
2022-07-24 16:16:30 -0700

    linting and clean-up

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -13,7 +13,7 @@ const Mission = () => {
         We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
       </Text>
     </Card>
-)
+  )
 }
 
 const CommunityPartners = () => {
@@ -22,21 +22,20 @@ const CommunityPartners = () => {
     <Card>
       <Card.Title>Community Partners</Card.Title>
       <Card.Divider />
-        {partners.map((partner) => {
-          return (
-            <ListItem key={partner.id}>
-              <Avatar source={partner.image} rounded/>
-              <ListItem.Content>
-                <ListItem.Title>{partner.name}</ListItem.Title>
-                <ListItem.Subtitle>
-                  {partner.description}
-                </ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          )
-        })}
+      {partners.map((partner) => {
+        return (
+          <ListItem key={partner.id}>
+            <Avatar source={partner.image} rounded/>
+            <ListItem.Content>
+              <ListItem.Title>{partner.name}</ListItem.Title>
+              <ListItem.Subtitle>
+                {partner.description}
+              </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        )
+      })}
     </Card>
-
   )
 }
 
